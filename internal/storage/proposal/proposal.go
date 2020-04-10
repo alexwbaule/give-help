411f47f998f94a32879fa03bea3899881d41138f
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-04-10 20:43:24 -0300

    Fix a lot os issues ans new features

--- internal/storage/proposal/proposal.go
+++ internal/storage/proposal/proposal.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"strings"
 
+	"log"
+
 	"github.com/alexwbaule/give-help/v2/generated/models"
 	"github.com/alexwbaule/give-help/v2/internal/common"
 	"github.com/alexwbaule/give-help/v2/internal/storage/connection"
 	"github.com/lib/pq"
-	"log"
 )
 
 //Proposal Object struct
@@ -27,14 +28,19 @@ INSERT INTO PROPOSALS (
     UserID,
     Side,
     ProposalType,
-    Tags,
+	Tags,
+	Title,
     Description,
     ProposalValidate,
     Lat,
     Long,
 	Range,
 	AreaTags,
-    IsActive
+	IsActive,	
+	Images,
+	DataToShare,
+	ExposeUserData,
+	EstimatedValue
 ) 
 VALUES
 (
@@ -42,28 +48,39 @@ VALUES
     $2, --UserID,
     $3, --Side,
     $4, --ProposalType,
-    $5, --Tags
-    $6, --Description
-    $7, --ProposalValidate,
-    $8, --Lat,
-    $9, --Long,
-    $10, --Range,
-	$11, --AreaTags
-    $12 --IsActive
+	$5, --Tags
+	$6, --Title
+    $7, --Description
+    $8, --ProposalValidate,
+    $9, --Lat,
+    $10, --Long,
+    $11, --Range,
+	$12, --AreaTags,
+	$13, --IsActive,
+	$14, --Images,
+	$15, --DataToShare,
+	$16, --ExposeUserData,
+	$17  --EstimatedValue,
 )
 ON CONFLICT (ProposalID) 
 DO UPDATE SET
 	LastUpdate = CURRENT_TIMESTAMP,
     Side = $3,
     ProposalType = $4,
-    Tags = $5,
-    Description = $6,
-    ProposalValidate = $7,
-    Lat = $8,
-    Long = $9,
-	Range = $10,
-	AreaTags = $11,
-    IsActive = $12;
+	Tags = $5,
+	Title = $6,
+    Description = $7,
+    ProposalValidate = $8,
+    Lat = $9,
+    Long = $10,
+	Range = $11,
+	AreaTags = $12,
+	IsActive = $13,	
+	Images = $14,
+	DataToShare = $15,
+	ExposeUserData = $16,
+	EstimatedValue = $17
+;
 `
 
 //Upsert insert or update on database
@@ -98,6 +115,14 @@ func (p *Proposal) Upsert(proposal *models.Proposal) error {
 		}
 	}
 
+	if proposal.DataToShare == nil {
+		proposal.DataToShare = []models.DataToShare{}
+	}
+
+	if proposal.Images == nil {
+		proposal.Images = []string{}
+	}
+
 	_, err := db.Exec(
 		upsertProposal,
 		proposal.ProposalID,
@@ -105,6 +130,7 @@ func (p *Proposal) Upsert(proposal *models.Proposal) error {
 		proposal.Side,
 		proposal.ProposalType,
 		pq.Array(proposal.Tags),
+		proposal.Title,
 		proposal.Description,
 		proposal.ProposalValidate,
 		lat,
@@ -112,6 +138,10 @@ func (p *Proposal) Upsert(proposal *models.Proposal) error {
 		areaRange,
 		pq.Array(areaTags),
 		proposal.IsActive,
+		pq.Array(proposal.Images),
+		pq.Array(proposal.DataToShare),
+		proposal.ExposeUserData,
+		proposal.EstimatedValue,
 	)
 
 	if err != nil {
@@ -134,13 +164,18 @@ SELECT
 	Side,
 	ProposalType,
 	Tags,
+	Title,
 	Description,
 	ProposalValidate,
 	Lat,
 	Long,
 	Range,
 	AreaTags,
-	IsActive
+	IsActive,
+	Images,
+	EstimatedValue,
+	ExposeUserData,
+	DataToShare
 FROM
 	PROPOSALS
 WHERE	
@@ -152,7 +187,10 @@ ORDER BY
 
 //LoadFromProposal load an unique proposal from a proposalID
 func (p *Proposal) LoadFromProposal(prposalID string) (*models.Proposal, error) {
-	ret := models.Proposal{TargetArea: &models.Area{}}
+	ret := models.Proposal{
+		TargetArea:  &models.Area{},
+		DataToShare: []models.DataToShare{},
+	}
 
 	cmd := fmt.Sprintf(selectProposal, "ProposalID = $1")
 
@@ -161,6 +199,8 @@ func (p *Proposal) LoadFromProposal(prposalID string) (*models.Proposal, error) 
 
 	var tags []string
 	var areaTags []string
+	var images []string
+	var dataToShare []string
 
 	err := db.QueryRow(cmd, prposalID).Scan(
 		&ret.ProposalID,
@@ -170,6 +210,7 @@ func (p *Proposal) LoadFromProposal(prposalID string) (*models.Proposal, error) 
 		&ret.Side,
 		&ret.ProposalType,
 		pq.Array(&tags),
+		&ret.Title,
 		&ret.Description,
 		&ret.ProposalValidate,
 		&ret.TargetArea.Lat,
@@ -177,10 +218,20 @@ func (p *Proposal) LoadFromProposal(prposalID string) (*models.Proposal, error) 
 		&ret.TargetArea.Range,
 		pq.Array(&areaTags),
 		&ret.IsActive,
+		pq.Array(&images),
+		&ret.EstimatedValue,
+		&ret.ExposeUserData,
+		pq.Array(&dataToShare),
 	)
 
 	ret.Tags = tags
 	ret.TargetArea.AreaTags = areaTags
+	ret.Images = images
+
+	ret.DataToShare = make([]models.DataToShare, len(dataToShare))
+	for i, v := range dataToShare {
+		ret.DataToShare[i] = models.DataToShare(v)
+	}
 
 	return &ret, p.conn.CheckError(err)
 }
@@ -204,28 +255,47 @@ func (p *Proposal) Find(filter *models.Filter) ([]*models.Proposal, error) {
 
 	if len(filter.Description) > 0 {
 		args = append(args, "%"+strings.ToUpper(filter.Description)+"%")
-		wheres = append(wheres, fmt.Sprintf("UPPER(Description) LIKE $%d", len(wheres)+1))
+		wheres = append(wheres, fmt.Sprintf("( UPPER(Description) LIKE $%d OR UPPER(Title) LIKE $%d )", len(args), len(args)))
+
+		for _, s := range strings.Split(filter.Description, " ") {
+			if len(s) > 0 {
+				args = append(args, strings.ToUpper(s))
+				wheres = append(wheres, fmt.Sprintf(" AreaTags && ARRAY[ $%d ]", len(args)))
+			}
+		}
+
 	}
 
 	if len(filter.Side) > 0 {
 		args = append(args, filter.Side)
-		wheres = append(wheres, fmt.Sprintf("Side = $%d", len(wheres)+1))
+		wheres = append(wheres, fmt.Sprintf("Side = $%d", len(args)))
 	}
 
 	for _, t := range filter.ProposalTypes {
 		args = append(args, t)
-		wheres = append(wheres, fmt.Sprintf("ProposalType = $%d", len(wheres)+1))
+		wheres = append(wheres, fmt.Sprintf("ProposalType = $%d", len(args)))
 	}
 
 	for _, t := range filter.Tags {
 		args = append(args, strings.ToUpper(t))
-		wheres = append(wheres, fmt.Sprintf("$%d = ANY(Tags)", len(wheres)+1))
+		wheres = append(wheres, fmt.Sprintf("$%d = ANY(Tags)", len(args)))
+	}
+
+	if filter.MaxValue > 0 {
+		args = append(args, filter.MinValue, filter.MaxValue)
+		wheres = append(wheres, fmt.Sprintf("(EstimatedValue >= $%d AND EstimatedValue <= $%d )", len(args)-1, len(args)))
+
+	}
+
+	if filter.MinValue > 0 {
+		args = append(args, filter.MinValue)
+		wheres = append(wheres, fmt.Sprintf("EstimatedValue >= $%d", len(args)))
 	}
 
 	if filter.TargetArea != nil {
 		for _, t := range filter.TargetArea.AreaTags {
 			args = append(args, strings.ToUpper(t))
-			wheres = append(wheres, fmt.Sprintf("$%d = ANY(AreaTags)", len(wheres)+1))
+			wheres = append(wheres, fmt.Sprintf("$%d = ANY(AreaTags)", len(args)))
 		}
 
 		rang := filter.TargetArea.Range
@@ -236,18 +306,15 @@ func (p *Proposal) Find(filter *models.Filter) ([]*models.Proposal, error) {
 			}
 
 			if N, S, W, E, err := common.CalculeRange(filter.TargetArea); err == nil {
-				args = append(args, S)
-				args = append(args, N)
-				args = append(args, E)
-				args = append(args, W)
+				args = append(args, S, N, E, W)
 				wheres = append(
 					wheres,
 					fmt.Sprintf(
 						`( (Lat BETWEEN $%d AND $%d) AND (Long BETWEEN $%d AND $%d ) )`,
-						len(wheres)+1,
-						len(wheres)+2,
-						len(wheres)+3,
-						len(wheres)+4),
+						len(args)-3,
+						len(args)-2,
+						len(args)-1,
+						len(args)),
 				)
 			}
 		} else {
@@ -276,8 +343,11 @@ func (p *Proposal) load(cmd string, args ...interface{}) ([]*models.Proposal, er
 
 	for rows.Next() {
 		i := models.Proposal{TargetArea: &models.Area{}}
+
 		var tags []string
 		var areaTags []string
+		var images []string
+		var dataToShare []string
 
 		err = rows.Scan(
 			&i.ProposalID,
@@ -287,6 +357,7 @@ func (p *Proposal) load(cmd string, args ...interface{}) ([]*models.Proposal, er
 			&i.Side,
 			&i.ProposalType,
 			pq.Array(&tags),
+			&i.Title,
 			&i.Description,
 			&i.ProposalValidate,
 			&i.TargetArea.Lat,
@@ -294,6 +365,10 @@ func (p *Proposal) load(cmd string, args ...interface{}) ([]*models.Proposal, er
 			&i.TargetArea.Range,
 			pq.Array(&areaTags),
 			&i.IsActive,
+			pq.Array(&images),
+			&i.EstimatedValue,
+			&i.ExposeUserData,
+			pq.Array(&dataToShare),
 		)
 
 		if err != nil {
@@ -302,6 +377,12 @@ func (p *Proposal) load(cmd string, args ...interface{}) ([]*models.Proposal, er
 
 		i.Tags = tags
 		i.TargetArea.AreaTags = areaTags
+		i.Images = images
+
+		i.DataToShare = make([]models.DataToShare, len(dataToShare))
+		for pos, v := range dataToShare {
+			i.DataToShare[pos] = models.DataToShare(v)
+		}
 
 		ret = append(ret, &i)
 	}
