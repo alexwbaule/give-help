680d21a1014394db7e8ace42a09a855e5b82d116
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-04-17 21:24:20 -0300

    Change get DataToShare

--- handlers/proposal/proposal.go
+++ handlers/proposal/proposal.go
@@ -9,18 +9,21 @@ import (
 	"github.com/alexwbaule/give-help/v2/internal/common"
 	"github.com/alexwbaule/give-help/v2/internal/storage/connection"
 	storage "github.com/alexwbaule/give-help/v2/internal/storage/proposal"
+	userStorage "github.com/alexwbaule/give-help/v2/internal/storage/user"
 	"github.com/go-openapi/strfmt"
 )
 
 //Proposal Object struct
 type Proposal struct {
-	storage *storage.Proposal
+	storage     *storage.Proposal
+	userStorage *userStorage.User
 }
 
 //New creates a new instance
 func New(conn *connection.Connection) *Proposal {
 	return &Proposal{
-		storage: storage.New(conn),
+		storage:     storage.New(conn),
+		userStorage: userStorage.New(conn),
 	}
 }
 
@@ -99,18 +102,60 @@ func (p *Proposal) Find(filter *models.Filter) ([]*models.Proposal, error) {
 }
 
 //GetUserDataToShare load user data to share on proposal
-func (p *Proposal) GetUserDataToShare(proposalID string) ([]models.DataToShare, error) {
+func (p *Proposal) GetUserDataToShare(proposalID string) ([]*models.DataToShareResponse, error) {
 	if len(proposalID) == 0 {
-		return []models.DataToShare{}, fmt.Errorf("proposalID is empty")
+		return []*models.DataToShareResponse{}, fmt.Errorf("proposalID is empty")
 	}
 
-	ret, err := p.Load(proposalID)
+	ret := []*models.DataToShareResponse{}
+
+	prop, err := p.Load(proposalID)
 
 	if err != nil {
 		log.Printf("fail to load proposal [%s]: %s", proposalID, err)
+		return ret, err
 	}
 
-	return ret.DataToShare, err
+	user, err := p.userStorage.Load(string(prop.UserID))
+
+	if err != nil {
+		log.Printf("fail to load user [%s]: %s", prop.UserID, err)
+		return ret, err
+	}
+
+	for _, dts := range prop.DataToShare {
+		if user.Contact != nil {
+			switch dts {
+			case models.DataToShareEmail:
+				ret = append(ret, &models.DataToShareResponse{
+					ContactType: models.DataToShareEmail,
+					Contact:     user.Contact.Email,
+				})
+			case models.DataToShareFacebook:
+				ret = append(ret, &models.DataToShareResponse{
+					ContactType: models.DataToShareFacebook,
+					Contact:     user.Contact.Facebook,
+				})
+			case models.DataToSharePhone:
+				ret = append(ret, &models.DataToShareResponse{
+					ContactType: models.DataToSharePhone,
+					Contact:     user.Contact.Phones,
+				})
+			case models.DataToShareInstagram:
+				ret = append(ret, &models.DataToShareResponse{
+					ContactType: models.DataToShareInstagram,
+					Contact:     user.Contact.Instagram,
+				})
+			case models.DataToShareURL:
+				ret = append(ret, &models.DataToShareResponse{
+					ContactType: models.DataToShareURL,
+					Contact:     user.Contact.URL,
+				})
+			}
+		}
+	}
+
+	return ret, err
 }
 
 //ChangeValidStatus change active field
