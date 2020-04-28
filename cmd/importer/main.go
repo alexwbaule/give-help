5cbe59f4ffd4d8a2bcdad4c0654119043ce69fa6
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-04-28 11:12:53 -0300

    Implement proposal importer

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -1,33 +1,48 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
+	"firebase.google.com/go/auth"
 	"github.com/360EntSecGroup-Skylar/excelize"
 
 	"github.com/alexwbaule/give-help/v2/generated/models"
+	proposalHandler "github.com/alexwbaule/give-help/v2/handlers/proposal"
+	userHandler "github.com/alexwbaule/give-help/v2/handlers/user"
+	"github.com/alexwbaule/give-help/v2/internal/common"
+	runtimeApp "github.com/alexwbaule/give-help/v2/runtime"
+	"github.com/alexwbaule/go-app"
 )
+
+type FirebaseUser struct {
+	Email       string
+	PhoneNumber string
+	Password    string
+	DisplayName string
+	PhotoURL    string
+	Line        int
+}
 
 func main() {
 	Execute(os.Args[1])
 }
 
 func Execute(filename string) {
-	/*
-		app, err := app.New("give-help-service")
+	app, err := app.New("give-help-service")
 
-		rt, err := runtimeApp.NewRuntime(app)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+	rt, err := runtimeApp.NewRuntime(app)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
-		defer rt.CloseDatabase()
-	*/
-	//userSvc := userHandler.New(rt.GetDatabase())
+	defer rt.CloseDatabase()
 
 	f, err := excelize.OpenFile(filename)
 
@@ -36,50 +51,69 @@ func Execute(filename string) {
 		return
 	}
 
-	importUsers("Users", f)
+	users, errs := importUsers(f, rt)
 
-	log.Printf("Done!")
+	for line, err := range errs {
+		log.Printf("[line %d] Error to try import user: %s\n", line, err)
+	}
+
+	if len(users) == 0 {
+		log.Println("[line %d] No users to import")
+	}
+
+	errs = importProps(users, f, rt)
+
+	for line, err := range errs {
+		log.Printf("[line %d] Error to try import proposal: %s\n", line, err)
+	}
+
+	log.Printf("Finish!")
 }
 
-func importUsers(sheetName string, f *excelize.File) map[int]error {
+func importUsers(f *excelize.File, rt *runtimeApp.Runtime) (map[string]*models.User, map[int]error) {
+	userIds := map[string]*models.User{}
+	errs := map[int]error{}
+
 	h := map[string]int{
-		"UserID":         0,
-		"Name":           0,
-		"Description":    0,
-		"Tags":           0,
-		"Images":         0,
-		"CreatedAt":      0,
-		"LastUpdate":     0,
-		"URL":            0,
-		"Email":          0,
-		"Facebook":       0,
-		"Instagram":      0,
-		"Google":         0,
-		"Address":        0,
-		"City":           0,
-		"State":          0,
-		"ZipCode":        0,
-		"Country":        0,
-		"Lat":            0,
-		"Long":           0,
-		"RegisterFrom":   0,
-		"PCountry":       0,
-		"PRegion":        0,
-		"PNumbers":       0,
-		"DeviceID":       0,
-		"AllowShareData": 0,
+		"UserID":         -1,
+		"Name":           -1,
+		"Description":    -1,
+		"Tags":           -1,
+		"Images":         -1,
+		"CreatedAt":      -1,
+		"LastUpdate":     -1,
+		"URL":            -1,
+		"Email":          -1,
+		"Facebook":       -1,
+		"Instagram":      -1,
+		"Google":         -1,
+		"Address":        -1,
+		"City":           -1,
+		"State":          -1,
+		"ZipCode":        -1,
+		"Country":        -1,
+		"Lat":            -1,
+		"Long":           -1,
+		"RegisterFrom":   -1,
+		"PCountry":       -1,
+		"PRegion":        -1,
+		"PNumbers":       -1,
+		"DeviceID":       -1,
+		"AllowShareData": -1,
 	}
 
-	rows := f.GetRows(sheetName)
+	rows := f.GetRows("Users")
 
 	if len(rows) < 2 {
-		err := fmt.Errorf("no lines to read")
+		err := fmt.Errorf("[Users] no lines to read")
 		log.Println(err)
 
-		return map[int]error{0: err}
+		errs[0] = err
+
+		return userIds, errs
 	}
 
-	ret := map[int]error{}
+	svc := userHandler.New(rt.GetDatabase())
 
 	for line, row := range rows {
 		//header
@@ -93,7 +127,7 @@ func importUsers(sheetName string, f *excelize.File) map[int]error {
 		}
 
 		//row
-		user := models.User{
+		user := &models.User{
 			Contact: &models.Contact{
 				Phones: []*models.Phone{},
 			},
@@ -103,30 +137,35 @@ func importUsers(sheetName string, f *excelize.File) map[int]error {
 			Tags:       models.Tags{},
 		}
 
-		user.UserID = models.UserID(row[h["UserID"]])
-		user.Name = row[h["Name"]]
-		user.Description = row[h["Description"]]
-		user.Tags = strings.Split(row[h["Tags"]], ",")
-		user.Images = strings.Split(row[h["Images"]], ",")
-		user.Contact.URL = row[h["URL"]]
-		user.Contact.Email = row[h["Email"]]
-		user.Contact.Facebook = row[h["Facebook"]]
-		user.Contact.Instagram = row[h["Instagram"]]
-		user.Contact.Google = row[h["Google"]]
-		user.Location.Address = row[h["Address"]]
-		user.Location.City = row[h["City"]]
-		user.Location.State = row[h["State"]]
-		user.Location.ZipCode = getInt(row[h["ZipCode"]])
-		user.Location.Country = row[h["Country"]]
-		user.Location.Lat = getFloat(row[h["Lat"]])
-		user.Location.Long = getFloat(row[h["Long"]])
-		user.RegisterFrom = row[h["RegisterFrom"]]
-		user.AllowShareData = getBool(row[h["AllowShareData"]])
+		sheetId := getData("UserID", h, row)
 
-		for _, phone := range strings.Split(row[h["PNumbers"]], ",") {
+		if len(sheetId) == 0 {
+			sheetId = common.GetULID()
+		}
+
+		user.Name = getData("Name", h, row)
+		user.Description = getData("Description", h, row)
+		user.Tags = strings.Split(getData("Tags", h, row), ",")
+		user.Images = strings.Split(getData("Images", h, row), ",")
+		user.Contact.URL = getData("URL", h, row)
+		user.Contact.Email = getData("Email", h, row)
+		user.Contact.Facebook = getData("Facebook", h, row)
+		user.Contact.Instagram = getData("Instagram", h, row)
+		user.Contact.Google = getData("Google", h, row)
+		user.Location.Address = getData("Address", h, row)
+		user.Location.City = getData("City", h, row)
+		user.Location.State = getData("State", h, row)
+		user.Location.ZipCode = getInt(getData("ZipCode", h, row))
+		user.Location.Country = getData("Country", h, row)
+		user.Location.Lat = getFloat(getData("Lat", h, row))
+		user.Location.Long = getFloat(getData("Long", h, row))
+		user.RegisterFrom = getData("RegisterFrom", h, row)
+		user.AllowShareData = getBool(getData("AllowShareData", h, row))
+
+		for _, phone := range strings.Split(getData("PNumbers", h, row), ",") {
 			user.Contact.Phones = append(user.Contact.Phones, &models.Phone{
-				CountryCode: row[h["PCountry"]],
-				Region:      row[h["PRegion"]],
+				CountryCode: getData("PCountry", h, row),
+				Region:      getData("PRegion", h, row),
 				PhoneNumber: phone,
 			})
 		}
@@ -135,14 +174,159 @@ func importUsers(sheetName string, f *excelize.File) map[int]error {
 			user.Contact.Phones[0].IsDefault = true
 		}
 
-		/*
-			//import user
-			if err != nil {
-				ret[i] = err
-			}
-		*/
+		//get firebase user id
+		id, err := upsertFirebase(line, user, rt)
+		if err != nil {
+			errs[line] = err
+			continue
+		}
+
+		user.UserID = models.UserID(id)
+
+		//insert user
+		_, err = svc.Insert(user, id)
+		if err != nil {
+			errs[line] = err
+			continue
+		}
+
+		userIds[sheetId] = user
+
+		log.Printf("[line %d] [API] User added\n", line)
 	}
 
+	return userIds, errs
+}
+
+func importProps(users map[string]*models.User, f *excelize.File, rt *runtimeApp.Runtime) map[int]error {
+	h := map[string]int{
+		"ProposalID":       -1,
+		"UserID":           -1,
+		"Title":            -1,
+		"Description":      -1,
+		"Side":             -1,
+		"ProposalType":     -1,
+		"Tags":             -1,
+		"IsActive":         -1,
+		"CreatedAt":        -1,
+		"LastUpdate":       -1,
+		"ProposalValidate": -1,
+		"AreaTags":         -1,
+		"Lat":              -1,
+		"Long":             -1,
+		"Range":            -1,
+		"Images":           -1,
+		"EstimatedValue":   -1,
+		"ExposeUserData":   -1,
+		"DataToShare":      -1,
+	}
+
+	rows := f.GetRows("Proposals")
+
+	if len(rows) < 2 {
+		err := fmt.Errorf("[proposals] no lines to read")
+		log.Println(err)
+
+		return map[int]error{0: err}
+	}
+
+	ret := map[int]error{}
+	svc := proposalHandler.New(rt.GetDatabase())
+
+	for line, row := range rows {
+		//header
+		if line == 0 {
+			for pos, name := range rows[0] {
+				if _, found := h[name]; found {
+					h[name] = pos
+				}
+			}
+			continue
+		}
+
+		//row
+		prop := &models.Proposal{
+			DataToShare: []models.DataToShare{},
+			Images:      []string{},
+			Tags:        models.Tags{},
+			TargetArea:  &models.Area{},
+			IsActive:    true,
+		}
+
+		sheetUserID := getData("UserID", h, row)
+
+		if len(sheetUserID) == 0 {
+			ret[line] = fmt.Errorf("[line %d] invalid user id", line)
+			continue
+		}
+
+		user, found := users[sheetUserID]
+
+		if !found {
+			ret[line] = fmt.Errorf("[line %d] user id (%s) not found!", line, sheetUserID)
+			continue
+		}
+
+		dts := []models.DataToShare{}
+
+		if len(user.Contact.Facebook) > 0 {
+			dts = append(dts, models.DataToShareFacebook)
+		}
+
+		if len(user.Contact.Instagram) > 0 {
+			dts = append(dts, models.DataToShareInstagram)
+		}
+
+		if len(user.Contact.Email) > 0 {
+			dts = append(dts, models.DataToShareEmail)
+		}
+
+		if len(user.Contact.URL) > 0 {
+			dts = append(dts, models.DataToShareURL)
+		}
+
+		if len(user.Contact.Phones) > 0 {
+			dts = append(dts, models.DataToSharePhone)
+		}
+
+		prop.DataToShare = dts
+
+		prop.Title = getData("Title", h, row)
+		prop.Description = getData("Description", h, row)
+
+		prop.Side = models.Side(getData("Side", h, row))
+		prop.ProposalType = models.Type(getData("ProposalType", h, row))
+
+		prop.Tags = strings.Split(getData("Tags", h, row), ",")
+		prop.Images = strings.Split(getData("Images", h, row), ",")
+
+		prop.TargetArea.Lat = getFloat(getData("Lat", h, row))
+		prop.TargetArea.Long = getFloat(getData("Long", h, row))
+		prop.TargetArea.Range = getFloat(getData("Range", h, row))
+		prop.TargetArea.AreaTags = strings.Split(getData("AreaTags", h, row), ",")
+
+		prop.EstimatedValue = getFloat(getData("EstimatedValue", h, row))
+		prop.ExposeUserData = getBool(getData("ExposeUserData", h, row))
+
+		//insert proposal
+		propId, err := svc.Insert(prop)
+		if err != nil {
+			ret[line] = err
+			continue
+		}
+
+		log.Printf("[line %d] [API] Proposal added (%s)\n", line, propId)
+	}
+
+	return ret
+}
+
+func getData(name string, h map[string]int, row []string) string {
+	pos := h[name]
+	ret := ""
+	if pos >= 0 {
+		ret = strings.TrimSpace(row[pos])
+	}
 	return ret
 }
 
@@ -168,4 +352,77 @@ func getBool(input string) bool {
 	}
 
 	return false
+}
+
+func parserToFirebaseUser(line int, user *models.User) FirebaseUser {
+	ret := FirebaseUser{Line: line}
+
+	if len(user.Contact.Email) > 0 {
+		ret.Email = user.Contact.Email
+	} else {
+		re := regexp.MustCompile(`[^A-Za-z0-9]`)
+		replaced := re.ReplaceAll([]byte(user.Name), []byte(""))
+
+		ret.Email = fmt.Sprintf("%s@%s.com", string(replaced), "give-help-importer")
+	}
+
+	ret.Password = ret.Email
+
+	if len(user.Contact.Phones) > 0 {
+		ret.PhoneNumber = fmt.Sprintf("%s%s%s", user.Contact.Phones[0].CountryCode, user.Contact.Phones[0].Region, user.Contact.Phones[0].PhoneNumber)
+	}
+
+	ret.DisplayName = user.Name
+
+	if len(user.Images) > 0 {
+		ret.PhotoURL = user.Images[0]
+	}
+
+	return ret
+}
+
+func upsertFirebase(line int, input *models.User, rt *runtimeApp.Runtime) (string, error) {
+	user := parserToFirebaseUser(line, input)
+
+	params := &auth.UserToCreate{}
+	params.Email(user.Email)
+	params.EmailVerified(false)
+
+	if len(user.PhoneNumber) > 0 {
+		params.PhoneNumber(user.PhoneNumber)
+	}
+
+	params.Password(user.Password)
+	params.DisplayName(user.DisplayName)
+
+	if len(user.PhotoURL) > 0 {
+		params.PhotoURL(user.PhotoURL)
+	}
+
+	params.Disabled(false)
+
+	ctx := context.Background()
+
+	client, err := rt.GetFirebase().Auth(ctx)
+	if err != nil {
+		log.Fatalf("[line %d] [firebase] Error getting Auth client: %v\n", line, err)
+	}
+
+	u, err := client.GetUserByEmail(ctx, user.Email)
+	if err == nil {
+		log.Printf("[line %d] [firebase] User (id=%s) already exists on firebase\n", line, u.UID)
+		return u.UID, err
+	}
+
+	u, err = client.CreateUser(ctx, params)
+
+	if err != nil {
+		dt, _ := json.MarshalIndent(user, "", "\t")
+		log.Fatalf("[line %d] [firebase] Error creating user: data%s\nerror: %s", line, string(dt), err)
+		return "", err
+	}
+
+	log.Printf("[line %d] [firebase] Successfully created user (id=%s)\n", line, u.UID)
+
+	return u.UID, err
 }
